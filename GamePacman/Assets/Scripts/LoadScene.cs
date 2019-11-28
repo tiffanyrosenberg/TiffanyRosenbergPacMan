265efbc68b732fb78ea036dc105fb741d129a072
tiffanyrosenberg <56071232+tiffanyrosenberg@users.noreply.github.com>
2019-11-28 15:57:37 +0100

    Task 2

---
+++ GamePacman/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public void LevelLoad(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        SceneManager.LoadScene(currentSceneIndex + 1);
+
+    }
+    public void QuitGame()
+    {
+
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+
+}
