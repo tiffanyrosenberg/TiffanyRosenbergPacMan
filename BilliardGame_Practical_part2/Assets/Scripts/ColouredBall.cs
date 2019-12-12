078cd874d81dcfd929c116bc05a76b5bef78ed9a
tiffanyrosenberg <56071232+tiffanyrosenberg@users.noreply.github.com>
2019-12-12 12:37:25 +0100

    Practical 2

---
+++ BilliardGame_Practical_part2/Assets/Scripts/ColouredBall.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColouredBall : MonoBehaviour
+{
+    void DestroyBallWhenItHitsHole(Collision2D col, CircleCollider2D Hole)
+    {
+
+        if(gameObject.name =="RightHole" || gameObject.name == "LeftHole")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
---
+++ BilliardGame_Practical_part2/Assets/Scripts/WhiteBall.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class WhiteBall : MonoBehaviour
+{ 
+    Vector3 White;
+    Rigidbody m_Rigidbody;
+
+    private class DestroyBallWhenItHitsHole(Collision2D col, CircleCollider2D Hole) = private new DestroyBallWhenItHitsHole(Collision2D col, CircleCollider2D Hole)
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetX();
+        GetY();
+        White = new Vector3(x, y);
+        m_Rigidbody = GetComponent<Rigidbody>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_Rigidbody.velocity = White;
+
+    }
+
+    float GetX(float x)
+    {
+
+        x = Random.Range(-1, 1);
+        
+
+        return x;
+    }
+
+    int GetY(int y)
+    {
+       
+        y = Random.Range(15, 20);
+
+       
+        return y;
+
+    }
+}
