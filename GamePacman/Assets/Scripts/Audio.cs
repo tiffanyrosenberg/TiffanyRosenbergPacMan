126a6c8d1e64668a3b4b492ce8b82d37a416bfaa
tiffanyrosenberg <56071232+tiffanyrosenberg@users.noreply.github.com>
2019-12-17 23:48:40 +0100

    Sound

---
+++ GamePacman/Assets/Scripts/Audio.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+    AudioSource audioData;
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioData = GetComponent<AudioSource>();
+        audioData.Play(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ GamePacman/Assets/Scripts/PacManSound.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacManSound : MonoBehaviour
+{
+    function OnTriggerEnter(hit : Collider)
+    {
+        if (hit.name == "PacMan" || hit.gameObject.tag == "PacMan")
+            audio.Play();
+    }
+
+}
