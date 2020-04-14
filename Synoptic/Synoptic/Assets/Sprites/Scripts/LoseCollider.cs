7c2ac1ba7e42a543a94b1d04f1a0037955a758cc
tiffanyrosenberg <56071232+tiffanyrosenberg@users.noreply.github.com>
2020-04-14 20:16:54 +0200

    Synoptic

---
+++ Synoptic/Synoptic/Assets/Sprites/Scripts/LoseCollider.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseCollider : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collider2D ball)
+    {
+        SceneManager.LoadScene("End");
+    }
+}
