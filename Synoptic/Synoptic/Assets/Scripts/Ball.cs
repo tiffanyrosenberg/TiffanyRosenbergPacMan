b9e27eb885e7c5fc4ec35983aa0fb57a3b17cc5b
tiffanyrosenberg <56071232+tiffanyrosenberg@users.noreply.github.com>
2020-04-14 19:15:42 +0200

    Synoptic

---
+++ Synoptic/Synoptic/Assets/Scripts/Ball.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] Paddle myPaddle;
+
+    //array declaration
+    [SerializeField] AudioClip[] ballSounds;
+
+    Vector2 paddleToBallDistance;
+
+    bool hasStarted = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBallDistance = this.transform.position - myPaddle.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted) // hasStarted == false
+        {
+            LockBallToPaddle();
+            LaunchBallOnClick();
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        //generate random number
+        int randomNumber = Random.Range(0, ballSounds.Length);
+
+        //read clip from array position = randomNumber
+        AudioClip myClip = ballSounds[randomNumber];
+
+        GetComponent<AudioSource>().PlayOneShot(myClip);
+    }
+
+    private void LaunchBallOnClick()
+    {
+        //if left click is pressed
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            //shoot the ball
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 15f);
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePosition = myPaddle.transform.position;
+
+        //set the Ball position to the paddlePosition + distance
+        this.transform.position = paddlePosition + paddleToBallDistance;
+    }
+}
---
+++ Synoptic/Synoptic/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadLevel(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        SceneManager.LoadScene(++currentSceneIndex);
+    }
+
+    public void QuitGame()
+    {
+        //Application.Quit();
+
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
+
